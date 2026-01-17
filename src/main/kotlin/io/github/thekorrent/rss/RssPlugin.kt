af35fee2d34d89fa5716b3bee9a2c6a298bbe1d1
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2026-01-17 12:08:45 +0800

    fix: premature initialization leads to NullPointerException

--- src/main/kotlin/io/github/thekorrent/rss/RssPlugin.kt
+++ src/main/kotlin/io/github/thekorrent/rss/RssPlugin.kt
@@ -11,17 +11,15 @@ import java.io.File
 class RssPlugin(wrapper: PluginWrapper) : KorrentPlugin(wrapper) {
     companion object {
         val pluginDataManager get() = _pluginDataManager
-        val config get() = _config
+        val config: RssConfig by lazy { _pluginConfigManager.load() }
 
         private var _pluginConfigManager = defaultNullPluginConfigManager
         private var _pluginDataManager = defaultNullPluginDataManager
-        private var _config = RssConfig()
     }
 
     init {
         _pluginConfigManager = this.pluginConfigManager
         _pluginDataManager = this.pluginDataManager
-        _config = _pluginConfigManager.load()
     }
 
     override fun start() {
